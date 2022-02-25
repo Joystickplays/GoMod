9e5a1b1e8cc7c3fdada218e21fa41adb6e47da2a
Joystickplays <joystickgaming09@gmail.com>
2022-02-25 21:41:21 +0900

    debug

--- cogs/moderation.py
+++ cogs/moderation.py
@@ -9,10 +9,8 @@ class Moderation(commands.Cog):
 
     @commands.Cog.listener()
     async def on_reaction_add(self, reaction, user):
-        print(1)
         lookup = await self.bot.db.fetchrow("SELECT * FROM reactroles WHERE message = $1 AND channel = $2", reaction.message.id, reaction.message.channel.id)
         if lookup:
-            print(2)
             if reaction.emoji.id == lookup['reaction']:
                 role = discord.utils.get(user.guild.roles, id=lookup['role'])
                 if role == None:
@@ -22,6 +20,7 @@ class Moderation(commands.Cog):
                     pass
                 else:
                     await user.add_roles(role)
+                    print("added successfully")
 
     @commands.command()
     @commands.has_guild_permissions(kick_members=True)
