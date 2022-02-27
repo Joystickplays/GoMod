623c32ab2df0ea4dc20f757a6fcd14420ab06383
Joystickplays <joystickgaming09@gmail.com>
2022-02-27 16:10:37 +0900

    idk

--- cogs/modrep.py
+++ cogs/modrep.py
@@ -4,6 +4,12 @@ import asyncio
 from bot import GoModBot
 from discord.ui import InputText, Modal
 
+async def getvotes(self, member):
+    upvotes = await self.bot.db.fetch("SELECT COUNT(*) FROM repvotes WHERE who = $1 AND type = 'up'", member.id)
+    downvotes = await self.bot.db.fetch("SELECT COUNT(*) FROM repvotes WHERE who = $1 AND type = 'down'", member.id)
+    votes = upvotes[0]["count"] - downvotes[0]["count"]
+    return discord.Embed(title="Reputation", description=f"{member.mention} has {votes} votes.", color=0x00b2ff)
+
 class UpDownvote(discord.ui.View):
     def __init__(self, bot, mem): 
         super().__init__()
@@ -22,15 +28,18 @@ class UpDownvote(discord.ui.View):
             if lookup["type"] == "down":
                 await self.bot.db.execute("DELETE FROM repvotes WHERE who = $1 AND voted = $2 AND type = 'down'", self.mem.id, interaction.user.id)
                 await self.bot.db.execute("INSERT INTO repvotes (who, voted, type) VALUES ($1, $2, 'up')", self.mem.id, interaction.user.id)
-                await interaction.response.send_message(content=f"{interaction.user.mention} cancelled the downvote and upvote this member.")
+                embed = await getvotes(self.mem)
+                await interaction.message.edit(content=f"Update: {interaction.user.mention} cancelled the downvote and upvote this member.", embed=embed)
                 return
             elif lookup["type"] == "up":
                 await self.bot.db.execute("DELETE FROM repvotes WHERE who = $1 AND voted = $2 AND type = 'up'", self.mem.id, interaction.user.id)
-                await interaction.response.send_message(f"{interaction.user.mention} cancelled the upvote for this user.")
+                embed = await getvotes(self.mem)
+                await interaction.message.edit(f"Update: {interaction.user.mention} cancelled the upvote for this user.", embed=embed)
                 return
 
         await self.bot.db.execute("INSERT INTO repvotes (who, voted, type) VALUES ($1, $2, 'up')", self.mem.id, interaction.user.id)
-        await interaction.response.send_message(content=f"{interaction.user.mention} upvoted this member.")
+        embed = await getvotes(self.mem)
+        await interaction.message.edit(content=f"Update: {interaction.user.mention} upvoted this member.", embed=embed)
 
     @discord.ui.button(label='Downvote', style=discord.ButtonStyle.red)
     async def downvote(self, button: discord.ui.Button, interaction: discord.Interaction):
@@ -42,16 +51,19 @@ class UpDownvote(discord.ui.View):
         if lookup:
             if lookup["type"] == "down":
                 await self.bot.db.execute("DELETE FROM repvotes WHERE who = $1 AND voted = $2 AND type = 'down'", self.mem.id, interaction.user.id)
-                await interaction.response.send_message(f"{interaction.user.mention} cancelled the downvote for this user.")
+                embed = await getvotes(self.mem)
+                await interaction.message.edit(f"Update: {interaction.user.mention} cancelled the downvote for this user.", embed=embed)
                 return
             elif lookup["type"] == "up":
                 await self.bot.db.execute("DELETE FROM repvotes WHERE who = $1 AND voted = $2 AND type = 'up'", self.mem.id, interaction.user.id)
                 await self.bot.db.execute("INSERT INTO repvotes (who, voted, type) VALUES ($1, $2, 'down')", self.mem.id, interaction.user.id)
-                await interaction.response.send_message(content=f"{interaction.user.mention} cancelled the upvote and downvoted this member.")
+                embed = await getvotes(self.mem)
+                await interaction.message.edit(content=f"Update: {interaction.user.mention} cancelled the upvote and downvoted this member.", embed=embed)
                 return
 
         await self.bot.db.execute("INSERT INTO repvotes (who, voted, type) VALUES ($1, $2, 'down')", self.mem.id, interaction.user.id)
-        await interaction.response.send_message(content=f"{interaction.user.mention} downvoted this member.")
+        embed = await getvotes(self.mem)
+        await interaction.message.edit(content=f"Update: {interaction.user.mention} downvoted this member.", embed=embed)
 
 
 class ModRep(commands.Cog):
@@ -60,10 +72,7 @@ class ModRep(commands.Cog):
 
     @commands.command()
     async def modrep(self, ctx, member: discord.Member):
-        upvotes = await self.bot.db.fetch("SELECT COUNT(*) FROM repvotes WHERE who = $1 AND type = 'up'", member.id)
-        downvotes = await self.bot.db.fetch("SELECT COUNT(*) FROM repvotes WHERE who = $1 AND type = 'down'", member.id)
-        votes = upvotes[0]["count"] - downvotes[0]["count"]
-        embed = discord.Embed(title="Reputation", description=f"{member.mention} has {votes} votes.", color=0x00b2ff)
+        embed = await getvotes(member)
         await ctx.send(embed=embed, view=UpDownvote(self.bot, member))
 
 def setup(bot:GoModBot):
